1144bb5eab7102dc233ff7d1d956ca045eea0392
Mikhail Bednarchuk <bednarchukmikhail@gmail.com>
2025-03-01 15:09:21 +0300

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Mikhail Bednarchuk on 01.03.2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+        Text("!@#")
+    }
+}
+
+#Preview {
+    HomeView()
+}
