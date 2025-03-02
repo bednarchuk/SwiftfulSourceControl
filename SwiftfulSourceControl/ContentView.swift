5e7055d5ebc9a107b6c67b4bb9d505672d26201c
Mikhail Bednarchuk <bednarchukmikhail@gmail.com>
2025-03-02 16:26:47 +0300

    [Fearure] Fix image

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "house.fill")
+            Image(systemName: "house")
                 .imageScale(.large)
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
